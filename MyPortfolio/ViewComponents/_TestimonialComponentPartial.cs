f79fed5925dd452417aa85dfb2328c5f0398bf43
emredurak-dev <officialemredurak@outlook.com>
2024-10-15 00:15:23 +0100

    .

---
+++ MyPortfolio/ViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _TestimonialComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }    
+            
+    }
+}
